3d47aa811b79d38d40fac0d2c5db68f419162a93
Marcus Battle <marcus@marcusbattle.com>
2013-10-02 17:59:59 -0400

    Added giving form

--- give.php
+++ give.php
@@ -12,34 +12,54 @@
 		</section>
 		<section>
 			<form method="POST">
-				<table>
+				<table id="contributions">
 					<thead>
 					    <tr>
-					      <th>Contribution</th>
-					      <th>Total</th>
+					    	<th></ht>
+					      	<th>Contribution</th>
+					      	<th>Total</th>
 					    </tr>
 				  	</thead>
-					<tr>
+					<tr class="contribution-row">
+						<td>
+							<a class="delete-row">[Delete]</a>
+						</td>
 						<td>
 							<select>
-								<option></option>
+								<option>--</option>
 								<option>Offering</option>
 								<option>Tithes</option>
 								<option>Gym Campaign</option>
 							</select>
 						</td>
 						<td>
-							<input type="text" placeholder="$100" />
+							<input type="text" name="total" placeholder="$100" />
 						</td>
 					</tr>
+					<tr class="total-row">
+						<td></td>
+						<td>Total</td>
+						<td id="total">$0</td>
+					</tr>
 					<tr>
-						<td><a class="add-contribution">Add new contribution</a></td>
+						<td></td>
+						<td colspan="2"><a class="add-contribution">Add new contribution</a></td>
 					</tr>
 				</table>
+				<p>&nbsp;</p>
+				<h2>Pay With</h2>
 				<ul>
-					<li></li>
-				</ul>
+					<li>
+						<select>
+							<option>Cash</option>
+							<option>Debit/Credit</option>
+							<option>Cash + Debit/Credit</option>
+						</select>
+					</li>
+				</li>
 			</form>
+			<p>You've given $5,346 this year. Thank you for your continued support and obedience to God's Word</p>
+			<p>- Malachi 3:10 -</p>
 		</section>
 		<section class="footer">
 			<h1 class="logo"><a href="#"><img src="img/newjc-seal.png"/></a></h1>
--- js/giving.js
+++ js/giving.js
@@ -1,12 +1,38 @@
 (function($){
 
-	$('.show-login, .show-signup').on('click', function(){
-		
+	$('.show-login, .show-signup').on('click', function(e){
+		e.preventDefault();
+
 		$('#intro').slideUp('slow');
 		$('section.dynamic').hide();
 
 		if ( $(this).hasClass('show-login') ) $('#login-view').show();
 		else if ( $(this).hasClass('show-signup') ) $('#signup-view').show();
+	});
+
+	$('a.add-contribution').on('click', function(e){
+		var row = $('.contribution-row:first-child').clone();
+
+		$('#contributions .contribution-row:first-child').after(row);
+	});
+
+	$('.delete-row').on('click', function(e){
+		console.log(  $(this) );
+		if ( $(this).closest('.contribution-row').not(':first-child') ) {
+			alert('right here');
+			$(this).closest('.contribution-row').remove();
+		}
+	});
+
+	$('input[name="total"').on('keyup', function(e){
+
+		var total = 0;
+
+		$('input[name="total"').each(function(){
+			total += $(this).val() * 1;
+		});
+
+		$('#total').text('$' + total);
 	});
 
 })(jQuery);
