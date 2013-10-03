20850ef39e929eccae83bbe2f9e2c6dfd1cd7d9f
Marcus Battle <marcus@marcusbattle.com>
2013-10-03 17:57:57 -0400

    Updated header

--- give.php
+++ give.php
@@ -5,65 +5,67 @@
 		<link href="css/giving.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-	<div class="main">
-		<h1><a href="/newjc-app">New Jerusalem Cathedral eGive</a></h1>
-		<section>
-			<p class="callout">Hi, <?php echo isset($_POST['first_name']) ? $_POST['first_name'] : 'Member' ?></p>
-			<p>Your giving goes towards our mission of resolving homelesness in Guilford County and Greater North Carolina.</p>
+		<section class="top">
+			<h1>eGive</h1>
 		</section>
-		<section>
-			<h2>Contributions</h2>
-			<form method="POST">
-				<table id="contributions">
-					<thead>
-					    <tr>
-					    	<th></ht>
-					      	<th></th>
-					      	<th>Amount</th>
-					    </tr>
-				  	</thead>
-					<tr class="contribution-row">
-						<td>
-							<a class="delete-row">[Delete]</a>
-						</td>
-						<td>
-							<select>
-								<option>--</option>
-								<option>Offering</option>
-								<option>Tithes</option>
-								<option>Gym Campaign</option>
-							</select>
-						</td>
-						<td>
-							<input type="text" name="amount[]" placeholder="$100" />
-						</td>
-					</tr>
-					<tr class="total-row">
-						<td></td>
-						<td>Total</td>
-						<td id="total">$0</td>
-					</tr>
-					<tr>
-						<td colspan="3"><a class="add-contribution">Add new contribution</a></td>
-					</tr>
-				</table>
+		<div class="main">
+			<section>
+				<p class="callout">Hi, <?php echo isset($_POST['first_name']) ? $_POST['first_name'] : 'Member' ?></p>
+				<p>Your giving goes towards our mission of resolving homelesness in Guilford County and Greater North Carolina.</p>
+			</section>
+			<section>
+				<h2>Contributions</h2>
+				<form method="POST">
+					<table id="contributions">
+						<thead>
+						    <tr>
+						    	<th></ht>
+						      	<th></th>
+						      	<th>Amount</th>
+						    </tr>
+					  	</thead>
+						<tr class="contribution-row">
+							<td>
+								<a class="delete-row">[Delete]</a>
+							</td>
+							<td>
+								<select>
+									<option>--</option>
+									<option>Offering</option>
+									<option>Tithes</option>
+									<option>Gym Campaign</option>
+								</select>
+							</td>
+							<td>
+								<input type="text" name="amount[]" placeholder="$100" />
+							</td>
+						</tr>
+						<tr class="total-row">
+							<td></td>
+							<td>Total</td>
+							<td id="total">$0</td>
+						</tr>
+						<tr>
+							<td colspan="3"><a class="add-contribution">Add new contribution</a></td>
+						</tr>
+					</table>
+					<p>&nbsp;</p>
+					<div class="button-group">
+						<a class="btn" href="add-credit-card.php">Add New Credit Card</a>
+						<button class="btn gold-btn" type="submit">Give with VISA 5017</button>
+					</div>
+				</form>
 				<p>&nbsp;</p>
-				<div class="button-group">
-					<a class="btn" href="add-credit-card.php">Add New Credit Card</a>
-					<button class="btn gold-btn" type="submit">Give with VISA 5017</button>
-				</div>
-			</form>
-			<p>&nbsp;</p>
-			<p>You've given <span class="amount-given">$5,346</span> this year. Thank you for your continued support and obedience to God's Word.</p>
-			<p>- Malachi 3:10 -</p>
-		</section>
-		<section class="footer">
-			<h1 class="logo"><a href="#"><img src="img/newjc-seal.png"/></a></h1>
-			<p>&copy 2013. New Jeruslaem Cahtedral. All Rights Reserved.</p>
-		</section>
-	</div>
+				<p>You've given <span class="amount-given">$5,346</span> this year. Thank you for your continued support and obedience to God's Word.</p>
+				<p>- Malachi 3:10 -</p>
+			</section>
+			<section class="footer">
+				<h1 class="logo"><a href="#"><img src="img/newjc-seal.png"/></a></h1>
+				<p>&copy 2013. New Jeruslaem Cahtedral. All Rights Reserved.</p>
+			</section>
+		</div>
 
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-	<script src="js/giving.js"></script>
-</body>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="js/giving.js"></script>
+	</body>
 </html>
--- index.php
+++ index.php
@@ -5,53 +5,56 @@
 		<link href="css/giving.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-	<div class="main">
-		<h1>New Jerusalem Cathedral eGive</h1>
-		<section>
-			<div class="button-group">
-				<a class="btn red-btn show-login">Login</a>
-				<a class="btn gold-btn show-signup">Signup</a>
-			</div>
-			<div id="intro">
-				<p class="callout">Here is the great message that will encourage people to give. Concise and motivating.</p>
-				<p>Here are more finite details of what this page and program are about. For more information visit newjc.org/give</p>
-			</div>
+		<section class="top">
+			<h1>New Jerusalem Cathedral eGive</h1>
 		</section>
-		<section id="login-view" class="dynamic">
-			<p class="callout">Login to give.</p>
-			<p>Or view your previous contributions.</p>
-			<form action="give.php" method="POST">
-				<ul>
-					<li><input type="text" placeholder="username" /></li>
-					<li><input type="password" placeholder="password" /></li>
-					<li class="button-group"><button class="btn">Log In</button></li>
-				</ul>
-			</form>
-		</section>
-		<section id="signup-view" class="dynamic">
-			<p class="callout">Create an account to give.</p>
-			<p>We promise not to sell/give your information to third parties.</p>
-			<form action="give.php" method="POST">
-				<ul>
-					<li><input type="text" name="username" placeholder="username" /></li>
-					<li><input type="text" name="first_name" placeholder="First Name" /></li>
-					<li><input type="text" name="last_name" placeholder="Last Name" /></li>
-					<li><input type="text" name="email" placeholder="Email" /></li>
-					<li><input type="password" name="password" placeholder="Password" /></li>
-					<li><input type="password" name="confirm_password" placeholder="Confirm Password" /></li>
-					<li><input type="text" name="birthday" placeholder="Birthday" /></li>
-					<li class="button-group"><button class="btn">Sign Up</button></li>
-				</ul>
-			</form>
-			<p class="disclaimer">NOTE: You must be 13yrs or older to use this system</p>
-		</section>
-		<section class="footer">
-			<h1 class="logo"><a href="#"><img src="img/newjc-seal.png"/></a></h1>
-			<p>&copy 2013. New Jeruslaem Cahtedral. All Rights Reserved.</p>
-		</section>
-	</div>
+		<div class="main">
+			
+			<section>
+				<div class="button-group">
+					<a class="btn red-btn show-login">Login</a>
+					<a class="btn gold-btn show-signup">Signup</a>
+				</div>
+				<div id="intro">
+					<p class="callout">Here is the great message that will encourage people to give. Concise and motivating.</p>
+					<p>Here are more finite details of what this page and program are about. For more information visit newjc.org/give</p>
+				</div>
+			</section>
+			<section id="login-view" class="dynamic">
+				<p class="callout">Login to give.</p>
+				<p>Or view your previous contributions.</p>
+				<form action="give.php" method="POST">
+					<ul>
+						<li><input type="text" placeholder="username" /></li>
+						<li><input type="password" placeholder="password" /></li>
+						<li class="button-group"><button class="btn">Log In</button></li>
+					</ul>
+				</form>
+			</section>
+			<section id="signup-view" class="dynamic">
+				<p class="callout">Create an account to give.</p>
+				<p>We promise not to sell/give your information to third parties.</p>
+				<form action="give.php" method="POST">
+					<ul>
+						<li><input type="text" name="username" placeholder="username" /></li>
+						<li><input type="text" name="first_name" placeholder="First Name" /></li>
+						<li><input type="text" name="last_name" placeholder="Last Name" /></li>
+						<li><input type="text" name="email" placeholder="Email" /></li>
+						<li><input type="password" name="password" placeholder="Password" /></li>
+						<li><input type="password" name="confirm_password" placeholder="Confirm Password" /></li>
+						<li><input type="text" name="birthday" placeholder="Birthday" /></li>
+						<li class="button-group"><button class="btn">Sign Up</button></li>
+					</ul>
+				</form>
+				<p class="disclaimer">NOTE: You must be 13yrs or older to use this system</p>
+			</section>
+			<section class="footer">
+				<h1 class="logo"><a href="#"><img src="img/newjc-seal.png"/></a></h1>
+				<p>&copy 2013. New Jeruslaem Cahtedral. All Rights Reserved.</p>
+			</section>
+		</div>
 
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-	<script src="js/giving.js"></script>
-</body>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="js/giving.js"></script>
+	</body>
 </html>
